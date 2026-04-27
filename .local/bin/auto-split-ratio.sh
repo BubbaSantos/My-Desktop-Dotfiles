fe2a64c2ffc03d7d494567665cb76fa6c52fe199
BubbaSantos <ultimatessambbo@gmail.com>
2026-04-27 12:30:59 +0100

    dotfiles: 2026-04-27 12:30

--- .local/bin/auto-split-ratio.sh
+++ .local/bin/auto-split-ratio.sh
@@ -9,7 +9,6 @@ handle() {
     if [[ ${1:0:12} == "openwindow>>" ]]; then
         workspace=$(hyprctl activeworkspace -j | jq '.id')
         count=$(hyprctl clients -j | jq "[.[] | select(.workspace.id == $workspace)] | length")
-
         if [[ "$count" -eq 1 ]]; then
             hyprctl keyword dwindle:default_split_ratio "$(get_ratio)"
         else
@@ -19,9 +18,7 @@ handle() {
 }
 
 sleep 2
-
 HISIG=$(ls /run/user/$(id -u)/hypr/ 2>/dev/null | head -n1)
-
 while true; do
     socat -u "UNIX-CONNECT:/run/user/$(id -u)/hypr/$HISIG/.socket2.sock" STDOUT | while read -r line; do
         handle "$line"
